60ceac1c8acc1a52b0f8acd6681946106009b526
Kian <sabbah128@gmail.com>
2022-08-07 06:35:13 -0700

    update

--- AntPost.py
+++ AntPost.py
@@ -5,20 +5,23 @@ import os
 folder_img = '.\\knee'
 imgs = os.listdir(folder_img)
 
-Knee_Ant_Post = 'Knee_Ant_Post'
-if not os.path.isdir(Knee_Ant_Post):
-    os.mkdir(Knee_Ant_Post)
+Knee_ant = 'knee_ant'
+Knee_post = 'knee_post'
+if not os.path.isdir(Knee_ant):
+    os.mkdir(Knee_ant)
+if not os.path.isdir(Knee_post):
+    os.mkdir(Knee_post)
 
 ant = post = 0
 for img in imgs:
     s = img.split('.')
     im = Image.open(folder_img + '\\' + img)
     if img.split('.')[-2] == '1':        
-        im.save(Knee_Ant_Post + '\\' + s[0]+'.'+s[1]+'.'+'ant'+'.jpg', mode='L')
+        im.save(Knee_ant + '\\' + s[0]+'.'+s[1]+'.'+'ant'+'.jpg', mode='L')
         ant += 1
     elif img.split('.')[-2] == '2':
         im = im.transpose(0)
-        im.save(Knee_Ant_Post + '\\' + s[0]+'.'+s[1]+'.'+'post'+'.jpg', mode='L')
+        im.save(Knee_post + '\\' + s[0]+'.'+s[1]+'.'+'post'+'.jpg', mode='L')
         post += 1
     else:
         print('unknown')
