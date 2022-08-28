4a25dde4e38e221fced8fe48cb5ac786ba723ebb
Kian <sabbah128@gmail.com>
2022-08-28 05:13:59 -0700

    start ankle

--- img_to_excle.py
+++ img_to_excle.py
@@ -3,33 +3,34 @@ import pandas as pd
 from PIL import Image
 
 
-# def change_name():
-#     os.mkdir('knee_name_ok')
-#     imgs = os.listdir('.\\knee_new')
-#     for i in imgs:
-#         sttr = i.split('.')        
-#         s = i.split('.')[0]
-#         s = s.replace('^', '_')
-#         s = s.upper()
-#         s = s+'.'+'.'.join(sttr[2:])
-#         im1 = Image.open('knee_new'+'\\'+ i)
-#         im1.save('.\\knee_name_ok'+'\\'+ s, mode='L')
+def change_name():
+    os.mkdir('ankle_name_ok')
+    imgs = os.listdir('.\\ankle')
+    for i in imgs:
+        sttr = i.split('.')        
+        s = i.split('.')[0]
+        s = s.replace('^', '_')
+        s = s.upper()
+        s = s+'.'+'.'.join(sttr[2:])
+        im1 = Image.open('ankle\\'+ i)
+        im1.save('.\\ankle_name_ok'+'\\'+ s, mode='L')
 
 
 def set_label():
     names = []
 
-    for images in os.listdir('knee_name_ok'):
+    for images in os.listdir('ankle_name_ok'):
         names.append(images.split('.')[0].lower())
 
     names = list(set(names))
     names.sort()
-    print('knee_name_ok : ', len(names))
+    print('ankle_name_ok : ', len(names))
 
     df = pd.DataFrame(names, columns=['names'])
-    writer = pd.ExcelWriter('knee_name_ok.xlsx')
-    df.to_excel(writer, sheet_name='knee_name_ok', index=False)
+    writer = pd.ExcelWriter('ankle_name_ok.xlsx')
+    df.to_excel(writer, sheet_name='ankle_name_ok', index=False)
     writer.save()
 
+change_name()
 set_label()
 print('Make excle file is done.')
