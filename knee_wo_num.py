18cdbed226c8a9e475ddaf0282b6bd369d9da568
Kian <sabbah128@gmail.com>
2022-08-30 11:11:27 -0700

    ankle

--- knee_wo_num.py
+++ knee_wo_num.py
@@ -2,24 +2,24 @@ from PIL import Image
 import os
 
 
-folder_img = '.\\knee'
+folder_img = '.\\ankle'
 imgs = os.listdir(folder_img)
 
-Knee_wo = 'Knee_wo'
+ankle_wo = 'ankle_wo'
 
-if not os.path.isdir(Knee_wo):
-    os.mkdir(Knee_wo)
+if not os.path.isdir(ankle_wo):
+    os.mkdir(ankle_wo)
 
 num = 0
 for img in imgs:
     s = img.split('.')
     im = Image.open(folder_img + '\\' + img)
     if img.split('.')[-2] == '1':        
-        im.save(Knee_wo + '\\' + s[0]+'.'+'ant'+'.jpg', mode='L')
+        im.save(ankle_wo + '\\' + s[0]+'.'+'ant'+'.jpg', mode='L')
         num += 1
     elif img.split('.')[-2] == '2':
         im = im.transpose(0)
-        im.save(Knee_wo + '\\' + s[0]+'.'+'post'+'.jpg', mode='L')
+        im.save(ankle_wo + '\\' + s[0]+'.'+'post'+'.jpg', mode='L')
         num += 1
     else:
         print('unknown')
